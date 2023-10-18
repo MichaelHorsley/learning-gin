1cddc383b63990124883d83f37df5dd9a3e487d0
Michael Horsley <38311326+MichaelHorsley@users.noreply.github.com>
2023-10-18 20:32:42 +0100

    Added a POST request example with a TODO as the message type, seems pretty straight forward

--- handlers/todo_controller.go
+++ handlers/todo_controller.go
@@ -8,10 +8,25 @@ import (
 
 func RegisterTodoHandlers(r *gin.Engine) {
 	r.GET("/ping", pingHandler)
+	r.POST("/todo", createTodoHandler)
 }
 
 func pingHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"message": "pong",
 	})
+}
+
+type CreateTodoRequest struct {
+	Title string `json:"title" binding:"required"`
+}
+
+func createTodoHandler(c *gin.Context) {
+	var todo CreateTodoRequest
+	if err := c.ShouldBindJSON(&todo); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{"todoId": "1", "title": todo.Title})
 }
